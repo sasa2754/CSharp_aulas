99d2d0eb823449672d0612eb0c6b1062c22eb0f3
sasa2754 <sasamor5@gmail.com>
2025-01-13 09:13:45 -0300

    first commit

---
+++ Aula 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Pode fazer assim para definir o arquivo principal, mas não é necessário, se já colocar o código direto ele já entende :)
+
+// public static class Program {
+//     public static void main(string[] args) {
+//         Console.WriteLine("Hello, World!");
+        
+//     }
+// }
+
+
+Console.WriteLine("Hello, World!");
+
